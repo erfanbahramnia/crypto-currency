c1cc271223fee3ebd138f427e07c31c4de66e46f
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-29 12:43:18 +0430

    the style of Crypto component completed

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
+import styles from "./Crypto.module.css"
+
 const Crypto = ({name, price, image, symbol, marketCap, priceChange}) => {
     return (
-        <div>
-            <div>
-                <img src={image} alt="coin-image" />
-                <h2>{name}</h2>
+        <div className={styles.container}>
+            <div className={styles.nameImage}>
+                <img src={image} alt="coin-image" className={styles.image} />
+                <h2 className={styles.name}>{name}</h2>
             </div>
-            <span>{symbol.toUpperCase()}</span>
-            <span>${price}</span>
-            <span>{priceChange}%</span>
-            <span>{marketCap}</span>
+            <span className={styles.symbol}>{symbol.toUpperCase()}</span>
+            <span className={styles.price}>${price}</span>
+            <span className={priceChange > 0 ? styles.priceChangePos : styles.priceChangeNeg}>{priceChange}%</span>
+            <span className={styles.market}>{marketCap}</span>
         </div>
     );
 };
